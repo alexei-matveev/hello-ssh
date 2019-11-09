50fb97bfa5b7362fc5fa5e92b7e0ee28cf0abf76
Alexei Matveev <alexei.matveev@gmail.com>
2019-11-09 17:33:44 +0100

    AM: try clj-ssh

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject hello-ssh "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Try SSH from Clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clj-commons/clj-ssh "0.5.15"]]
   :repl-options {:init-ns hello-ssh.core}
   :main hello-ssh.core)
--- src/hello_ssh/core.clj
+++ src/hello_ssh/core.clj
@@ -1,6 +1,15 @@
-(ns hello-ssh.core)
+(ns hello-ssh.core
+  (:require [clj-ssh.cli :as ssh]))
 
 (defn -main
   "I don't do a whole lot."
   []
-  (println "Hello, World!"))
+  ;; Your ~/.ssh/known_hosts will be full of new entries:
+  (ssh/default-session-options {:strict-host-key-checking :no})
+  ;; Everything  in the  .localhost  domain resolves  to localhost  on
+  ;; Ubuntu. It takes ~5 Seconds for 10 logins here ...
+  (time
+   (let [ids (for [x (range 10)
+                   :let [host (str "prefix-" x ".localhost")]]
+               (ssh/ssh host "id"))]
+     (println ids))))
