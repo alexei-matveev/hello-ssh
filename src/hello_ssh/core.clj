357ce07ce12c365684c9897d72ab61a7825e7dc2
Alexei Matveev <alexei.matveev@gmail.com>
2019-11-09 18:09:50 +0100

    AM: same n

--- src/hello_ssh/core.clj
+++ src/hello_ssh/core.clj
@@ -3,14 +3,16 @@
             [clj-ssh.ssh :as ssh]))
 
 (defn- many-hosts [n]
-  ;; Everything  in the  .localhost  domain resolves  to localhost  on
-  ;; Ubuntu. It takes ~5 Seconds for 10 logins here ...
+  ;; Simple  REPL  from  clj-ssh.cli   namespace.  Everything  in  the
+  ;; .localhost domain resolves to localhost on Ubuntu.
   (doall
    (for [x (range n)
          :let [host (str "prefix-" x ".localhost")]]
      (cli/ssh host "id"))))
 
 (defn- many-cmds [n]
+  ;; Note that  in this case one  has to work with  functions & macros
+  ;; from the clj-ssh.ssh namespace:
   (let [agent (ssh/ssh-agent {})
         session (ssh/session agent "localhost" {:strict-host-key-checking :no})]
      (ssh/with-connection session
@@ -21,8 +23,8 @@
 (defn -main []
   ;; Your ~/.ssh/known_hosts will be full of new entries:
   (cli/default-session-options {:strict-host-key-checking :no})
-  (time (println (many-hosts 10)))
-  ;; Try 10 Times the same on a single session and it takes 1.5s. Note
-  ;; that in  this case one has  to work with functions  & macros from
-  ;; the clj-ssh.ssh namespace:
-  (time (println (many-cmds 10))))
+  (let [n 10]
+    ;; It takes ~5 Seconds for 10 logins here ...
+    (time (println (many-hosts n)))
+    ;; OTOH 10 commands  the same on a single session  take 1.5s.
+    (time (println (many-cmds n)))))
